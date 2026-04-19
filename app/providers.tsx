ac57bb76c75159c0bdbdc712e95267ce86774bf5
keneanalemayhu <keneanalemayhu1@gmail.com>
2026-04-19 12:51:13 +0300

    feat: removed themeprovider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,35 +1,8 @@
 // @/app/providers.tsx
 
 "use client";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { CalendarModeProvider } from "@/components/context/CalendarModeContext";
-import { type ThemeProviderProps } from "next-themes";
-import { Toaster } from "@/components/ui/sonner";
 
-export function Providers({ children, ...props }: ThemeProviderProps) {
-  return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      storageKey="jireh-theme"
-      themes={["light", "dark", "system"]}
-      value={{
-        light: "light",
-        dark: "dark",
-        system: "system",
-      }}
-      {...props}
-    >
-      <CalendarModeProvider>
-        {children}
-        <Toaster
-          position="bottom-right"
-          expand={false}
-          richColors
-          closeButton
-        />
-      </CalendarModeProvider>
-    </NextThemesProvider>
-  );
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <CalendarModeProvider>{children}</CalendarModeProvider>;
 }
